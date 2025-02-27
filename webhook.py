9ff223559f213b574aadcaa783e4d7e95347e3c6
MohamedBourina19 <mohamedbougrina19@gmail.com>
2025-02-27 17:21:04 +0000

    Update webhook.py

--- webhook.py
+++ webhook.py
@@ -1,33 +1,38 @@
 from flask import Flask
-from flask import request
+from flask import request 
 from flask import jsonify
-import requests 
-import json
-VERIFY_TOKEN = "mohamedbougrina19"
+import requests
 
 app = Flask(__name__)
-@app.route("/", methods=["GET"])
-def message():return jsonify({"message":"error api"})
+
+url = "https://graph.facebook.com/v21.0/me/messages"
+VERIFY_TOKEN = "chatbot"
+
+@app.route("/", methods=["GET","POST"])
+def index():
+  return jsonify({"message":"error api"}),404
 @app.route("/webhook", methods=["GET"])
-def webhook():
-   if request.method == "GET":
-      model = request.args.get("hub.model")
-      token = request.args.get("hub.verify_token")
-      challenge = request.args.get("hub.challenge")
-      if model == "subscribe" and token == VERIFY_TOKEN:
-         return jsonify({"message": challenge})
-      else:return jsonify({"message":"error data request"})
+def GET_WEBHOOK():
+  mode = request.args.get("hub.mode")
+  token = request.args.get("hub.verify_token")
+  challenge = request.args.get("hub.challenge")
+  if mode == "subscribe" and token == "chatbot":
+    return challenge
+  else:
+    return jsonify({"message":"error args"})
 @app.route("/webhook", methods=["POST"])
-def webhooks():        
-   if request.method == "POST":
-      data =  request.get_json()
-      if data["object"] == "page":
-         id = data["entry"][0]["messaging"][0]["sender"]["id"]
-         msg = data["entry"][0]["messaging"][0]["message"]["text"]
-         url = "https://graph.facebook.com/v21.0/me/messages"
-         header = {"Authorization": "Bearer EAAQQA1jZB5X4BO9gulIGruLuSNQZBK4nLBecjEmZBprer0huHjHEb9RHg6GJh686AwcSZAe4LwlT34Qxbpyj6XZBpWSRN3ZB1jcqJ12ZCGZBHdnvifiZBSyCEjbaqRjZBFtjts9iAFIjdHPMQ0ZBhZA62IdYnaXHyCwy1iTsY8yUiAesRwJiRZAV7GVixPvdPCgNb2J5x4gZDZD"}
-         payload = {"message": {"text": msg},"recipient": {"id": id}}
-         requests.post(url, json=payload,headers=header)
-         break
+def POST_WEBHOOK():
+  data = request.get_json()
+  if data["object"] == "page":
+    id = data["entry"][0]["messaging"][0]["sender"]["id"]
+    msg = data["entry"][0]["messaging"][0]["message"]["text"]
+    header = {"Authorization": "Bearer EAAQQA1jZB5X4BO2xMCZCOEMpjj8sZANib8YPx1hGBp8EVRAATtLCbV041Wke1Y3K0fEF6n0h3ZBEkGm7wwoENo51XZB1UipOVcn2mWDNjSfbsj2q7f7gKTZA27RyWpz6yLjnRWM6PxwiDrD8qDXbQu72UZCDoMJS9ZCSGEyZBIZASxP2ZBweZCKNxxPAdqqCiwLrsKIpRAZDZD"}
+    payload = {"message": {"text": msg},"recipient": {"id": f"{id}"}}
+    requests.post(url, json=payload,headers=header)
 if __name__ == "__main__":
-   app.run(host="0.0.0.0", port=5000, debug=True) 
+  app.run(host="0.0.0.0", port=5000, debug=True)
+ 
+
+
+
+
