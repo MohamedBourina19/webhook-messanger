c44799d2e2b4a4e40e4077ff30365b86248c33e1
MohamedBourina19 <mohamedbougrina19@gmail.com>
2025-02-27 16:04:28 +0000

    Update webhook.py

--- webhook.py
+++ webhook.py
@@ -6,9 +6,9 @@ import json
 VERIFY_TOKEN = "mohamedbougrina19"
 
 app = Flask(__name__)
-@app.route("/", methods=["GET","POST"])
+@app.route("/", methods=["GET"])
 def message():return jsonify({"message":"error api"})
-@app.route("/webhook", methods=["GET","POST"])
+@app.route("/webhook", methods=["GET"])
 def webhook():
    if request.method == "GET":
       model = request.args.get("hub.model")
@@ -17,6 +17,8 @@ def webhook():
       if model == "subscribe" and token == VERIFY_TOKEN:
          return jsonify({"message": challenge})
       else:return jsonify({"message":"error data request"})
+@app.route("/webhook", methods=["POST"])
+def webhooks():        
    if request.method == "POST":
       data =  request.get_json()
       if data["object"] == "page":
@@ -26,6 +28,6 @@ def webhook():
          header = {"Authorization": "Bearer EAAQQA1jZB5X4BO9gulIGruLuSNQZBK4nLBecjEmZBprer0huHjHEb9RHg6GJh686AwcSZAe4LwlT34Qxbpyj6XZBpWSRN3ZB1jcqJ12ZCGZBHdnvifiZBSyCEjbaqRjZBFtjts9iAFIjdHPMQ0ZBhZA62IdYnaXHyCwy1iTsY8yUiAesRwJiRZAV7GVixPvdPCgNb2J5x4gZDZD"}
          payload = {"message": {"text": msg},"recipient": {"id": id}}
          requests.post(url, json=payload,headers=header)
-       
+         break
 if __name__ == "__main__":
    app.run(host="0.0.0.0", port=5000, debug=True) 
