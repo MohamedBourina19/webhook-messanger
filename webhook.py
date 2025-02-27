3137984341a01d17fe560c989c7e391cc1f594cb
MohamedBourina19 <mohamedbougrina19@gmail.com>
2025-02-27 19:06:11 +0000

    Update webhook.py

--- webhook.py
+++ webhook.py
@@ -7,25 +7,18 @@ app = Flask(__name__)
 
 url = "https://graph.facebook.com/v21.0/me/messages"
 VERIFY_TOKEN = "chatbot"
-
-@app.route("/", methods=["GET","POST"])
-def index():
-  return jsonify({"message":"error api"}),404
-@app.route("/webhook", methods=["GET"])
-def GET_WEBHOOK():
-  mode = request.args.get("hub.mode")
-  token = request.args.get("hub.verify_token")
-  challenge = request.args.get("hub.challenge")
-  if mode == "subscribe" and token == "chatbot":
-    return challenge
-  else:
-    return jsonify({"message":"error args"})
 @app.route("/webhook", methods=["POST"])
 def POST_WEBHOOK():
   data = request.get_json()
   if data["object"] == "page":
     id = data["entry"][0]["messaging"][0]["sender"]["id"]
     msg = data["entry"][0]["messaging"][0]["message"]["text"]
+    payload = {"recipient": {"id": f"{id}"},"sender_action": "mark_seen"}
+    requests.post(url, json=payload, headers=header)
+    payload = {"recipient": {"id": f"{id}"},"sender_action": "typing_on"}
+    requests.post(url, json=payload, headers=header)
+    payload = {"recipient": {"id": f"{id}"},"sender_action": "typing_off"}
+    requests.post(url, json=payload, headers=header)
     header = {"Authorization": "Bearer EAAQQA1jZB5X4BO2xMCZCOEMpjj8sZANib8YPx1hGBp8EVRAATtLCbV041Wke1Y3K0fEF6n0h3ZBEkGm7wwoENo51XZB1UipOVcn2mWDNjSfbsj2q7f7gKTZA27RyWpz6yLjnRWM6PxwiDrD8qDXbQu72UZCDoMJS9ZCSGEyZBIZASxP2ZBweZCKNxxPAdqqCiwLrsKIpRAZDZD"}
     payload = {"message": {"text": msg},"recipient": {"id": f"{id}"}}
     requests.post(url, json=payload,headers=header)
