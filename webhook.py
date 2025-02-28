81b9a47bd222d898fb411291c15415c51b562f4f
MohamedBourina19 <mohamedbougrina19@gmail.com>
2025-02-28 18:28:12 +0000

    Update webhook.py

--- webhook.py
+++ webhook.py
@@ -2,50 +2,42 @@ import requests
 from flask import Flask
 from flask import request
 from flask import jsonify
-import json
 
 ACCESS_TOKEN = "EAAQQA1jZB5X4BO2xMCZCOEMpjj8sZANib8YPx1hGBp8EVRAATtLCbV041Wke1Y3K0fEF6n0h3ZBEkGm7wwoENo51XZB1UipOVcn2mWDNjSfbsj2q7f7gKTZA27RyWpz6yLjnRWM6PxwiDrD8qDXbQu72UZCDoMJS9ZCSGEyZBIZASxP2ZBweZCKNxxPAdqqCiwLrsKIpRAZDZD"
-
-def message_vu(sender_id):
-    url = f"https://graph.facebook.com/v12.0/me/messages?access_token={ACCESS_TOKEN}"
-    headers = {"Content-Type": "application/json"}
-    data = {"recipient": {"id": sender_id}, "sender_action": "mark_seen"}
-    requests.post(url, headers=headers, data=json.dumps(data))
-
-def send_typing(sender_id):
-    url = f"https://graph.facebook.com/v12.0/me/messages?access_token={ACCESS_TOKEN}"
-    headers = {"Content-Type": "application/json"}
-    data = {"recipient": {"id": sender_id}, "sender_action": "typing_on"}
-    requests.post(url, headers=headers, data=json.dumps(data))
-
-def send_message(sender_id, message):
-    url = f"https://graph.facebook.com/v12.0/me/messages?access_token={ACCESS_TOKEN}"
-    headers = {"Content-Type": "application/json"}
-    data = {"recipient": {"id": sender_id}, "message": {"text": message}}
-    requests.post(url, headers=headers, data=json.dumps(data))
 
 app = Flask(__name__)
 @app.route("/", methods=["GET","POST"])
 def index():
-   return jsonify({"message":"error api !"})
+  return jsonify({"message":"error api"}),404
 @app.route("/webhook", methods=["GET"])
-def dashboard():
-   mode = request.args.get("hub.mode")
-   token = request.args.get("hub.verify_token")
-   challenge = request.args.get("hub.challenge")
-   if mode == "subscribe":
-      return jsonify({"message":challenge}),200
-   else:
-      return jsonify({"message":"error from webhook"}),404
+def settings():
+  if request.method == "GET":
+    mode = request.args.get("hub.mode")
+    challenge = request.args.get("hub.challenge")
+    verify_token = request.args.get("hub.verify_token")
+    if mode == "subscribe":
+      return challenge
 @app.route("/webhook", methods=["POST"])
 def webhook():
-   body = request.get_json()
-   if body["object"]=="page":
-       message = data["entry"][0]["messaging"][0]["message"]
-       sender_id = data["entry"][0]["messaging"][0]["sender"]["id"]
-       send_typing(sender_id)
-       message_vu(sender_id)
-       send_message(message)  
+  body = request.get_json()
+  if body["object"] == "page":
+    id = ["entry"][0]["messaging"][0]["sender"]["id"]
+    msg = ["entry"][0]["messaging"][0]["message"]["text"] 
+    url = f"https://graph.facebook.com/v12.0/me/messages?access_token={ACCESS_TOKEN}"
+    headers = {"Content-Type": "application/json"}
+    data = {"recipient": {"id": id}, "sender_action": "mark_seen"}
+    requests.post(url, headers=headers, data=json.dumps(data))
+    data = {"recipient": {"id":id}, "sender_action": "typing_on"}
+    requests.post(url, headers=headers, data=json.dumps(data))
+    headers = {"Content-Type": "application/json"}
+    data = {"recipient": {"id":id}, "message": {"text": msg}}
+    requests.post(url, headers=headers, data=json.dumps(data))
 
 if __name__ == "__main__":
-   app.run(host="0.0.0.0", port=5000, debug=True)              
+  app.run(host="0.0.0.0", port=5000, debug=True)
+
+
+
+
+
+
